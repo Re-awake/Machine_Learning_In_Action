007db9d2a6c957d314ecd0b4322cad5b04da7113
kungbob <kung.bob@gmail.com>
2018-09-02 17:19:43 +0800

    Add biKmeans function

--- chapter10/10_1.py
+++ chapter10/10_1.py
@@ -1,7 +1,11 @@
 import kMeans
+import os
+import sys
 from numpy import *
 
-datMat = mat(kMeans.loadDataSet('testSet.txt'))
+project_path = os.path.abspath(os.path.dirname(__file__))
+text_path = os.path.join(project_path, "../chapter10/testSet.txt")
+datMat = mat(kMeans.loadDataSet(text_path))
 print(min(datMat[:, 0]))
 print(min(datMat[:, 1]))
 print(max(datMat[:, 1]))
--- chapter10/10_2.py
+++ chapter10/10_2.py
@@ -1,5 +1,9 @@
 import kMeans
+import os
+import sys
 from numpy import *
 
-datMat = mat(kMeans.loadDataSet('testSet.txt'))
+project_path = os.path.abspath(os.path.dirname(__file__))
+text_path = os.path.join(project_path, "../chapter10/testSet.txt")
+datMat = mat(kMeans.loadDataSet(text_path))
 myCentroids, clustAssing = kMeans.kMeans(datMat, 4)
